568733596d62728d184b29d832f248b3a9c8ee98
Dayne Dewar <daynedewar@yahoo.com>
2021-02-16 13:42:55 -0500

    fixing login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_customer, :is_logged_in?
+
+    def current_customer
+        Customer.find_by(id: session[:customer_id])
+    end
+
+    def is_logged_in?
+        !!current_customer
+    end
+
+    def authorized
+        redirect_to "/login" unless is_logged_in?
+    end
+    
 end
